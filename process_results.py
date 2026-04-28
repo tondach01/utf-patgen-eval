09f0eb4244277b715b6bc265231ce172f375ca83
Ondřej Metelka <xmetelk1@fi.muni.cz>
2026-04-28 08:29:56 +0200

    fix: exclude failing experiments

--- process_results.py
+++ process_results.py
@@ -1,12 +1,30 @@
 import pandas as pd
 
 def process_time(data: pd.DataFrame):
-    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"])["UserTime(s)"].agg(["min", "max", "mean", "std"]))
-    grouped.to_csv("time.csv")
+    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"], as_index=False)["UserTime(s)"].agg(["min", "max", "mean", "std"]))
+
+    # exclude experiments that failed with output limit exception
+    grouped = grouped[
+        (grouped["Binary"] != "patgen") |
+        (grouped["Profile"] == "cshyphen.in") |
+        (grouped["Profile"] == "wortliste.in") |
+        (grouped["Dataset"] != "de_wortliste")
+    ].round({"std": 2})
+
+    grouped.to_csv("time.csv", index=False)
 
 def process_memory(data: pd.DataFrame):
-    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"])["PeakMemory(KB)"].agg(["min", "max", "mean", "std"]))
-    grouped.to_csv("memory.csv")
+    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"], as_index=False)["PeakMemory(KB)"].agg(["min", "max", "mean", "std"]))
+    
+    # exclude experiments that failed with output limit exception
+    grouped = grouped[
+        (grouped["Binary"] != "patgen") |
+        (grouped["Profile"] == "cshyphen.in") |
+        (grouped["Profile"] == "wortliste.in") |
+        (grouped["Dataset"] != "de_wortliste")
+    ].round({"std": 2})
+    
+    grouped.to_csv("memory.csv", index=False)
 
 def check_correctness(data: pd.DataFrame):
     grouped = data.groupby(["Profile", "Dataset"], as_index=False)[["Good", "Bad", "Missed", "Patterns"]].agg(
@@ -27,6 +45,25 @@ def check_correctness(data: pd.DataFrame):
     ]
     grouped.to_csv("correctness.csv", index=False)
 
+def check_stability(data: pd.DataFrame):
+    grouped = data.groupby(["Binary", "Profile", "Dataset"], as_index=False)[["Good", "Bad", "Missed", "Patterns"]].agg(
+        Good_min=("Good", "min"),
+        Good_max=("Good", "max"),
+        Bad_min=("Bad", "min"),
+        Bad_max=("Bad", "max"),
+        Missed_min=("Missed", "min"),
+        Missed_max=("Missed", "max"),
+        Patterns_min=("Patterns", "min"),
+        Patterns_max=("Patterns", "max")
+    )
+    grouped = grouped[
+        (grouped["Good_min"] != grouped["Good_max"]) |
+        (grouped["Bad_min"] != grouped["Bad_max"]) |
+        (grouped["Missed_min"] != grouped["Missed_max"]) |
+        (grouped["Patterns_min"] != grouped["Patterns_max"])
+    ]
+    grouped.to_csv("stability.csv", index=False)
+
 def main():
     data = pd.read_csv("evaluation_results.csv")
     
@@ -35,6 +72,8 @@ def main():
     process_memory(data)
 
     check_correctness(data)
+
+    check_stability(data)
 
 if __name__ == "__main__":
     main()
