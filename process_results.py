895243e83a78952ff26e301beb06121f9baa75c6
Ondřej Metelka <xmetelk1@fi.muni.cz>
2026-04-29 07:35:45 +0200

    table: add filtering

--- process_results.py
+++ process_results.py
@@ -1,30 +1,52 @@
 import pandas as pd
+import typing
+
+def process_column(data: pd.DataFrame,
+                   column_name: str,
+                   export_name: str,
+                   filter_dataset: typing.Union[str, None] = None,
+                   filter_profile: typing.Union[str, None] = None):
+    grouped = data.groupby(["Binary", "Profile", "Dataset"], as_index=False)[column_name].agg(["mean", "std"])
+
+    if filter_dataset is not None:
+        grouped = grouped[grouped["Dataset"] == filter_dataset]
+    if filter_profile is not None:
+        grouped = grouped[grouped["Profile"] == filter_profile]
+    
+    # exclude experiments that failed with output limit exception and round std
+    grouped = grouped[
+        (grouped["Binary"] != "patgen") |
+        (grouped["Profile"] == "cshyphen.in") |
+        (grouped["Profile"] == "wortliste.in") |
+        (grouped["Dataset"] != "de_wortliste")
+    ].round({"std": 2, "mean": 2}) # pyright: ignore[reportArgumentType]
+    
+    grouped_patgen = grouped[grouped["Binary"] == "patgen"].drop(columns=["Binary"])
+    grouped_utfpatgen = grouped[grouped["Binary"] == "utfpatgen"].drop(columns=["Binary"])
+
+    ratios = pd.merge(grouped_patgen, grouped_utfpatgen, on=["Profile", "Dataset"], suffixes=("_p", "_u"))
+    ratios["\\patgen"] = ratios.apply(lambda row: f"{row['mean_p']} ({row['std_p']})", axis=1)
+    ratios["\\utfpatgen"] = ratios.apply(lambda row: f"{row['mean_u']} ({row['std_u']})", axis=1)
+    ratios["Ratio"] = ratios["mean_u"] / ratios["mean_p"]
+    
+    ratios = ratios.drop(columns=["mean_p", "std_p", "mean_u", "std_u"]).round({"Ratio": 3})
+    sort_columns = ["Dataset", "Profile", "Ratio"]
+    if filter_dataset is not None:
+        ratios = ratios.drop(columns=["Dataset"])
+        sort_columns.remove("Dataset")
+    if filter_profile is not None:
+        ratios = ratios.drop(columns=["Profile"])
+        sort_columns.remove("Profile")
+    ratios = ratios.sort_values(by=sort_columns)
+    ratios = ratios[["Dataset", "Profile", "\\patgen", "\\utfpatgen", "Ratio"]]
+
+    ratios.to_csv(f"{export_name}.csv", index=False)
 
 def process_time(data: pd.DataFrame):
-    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"], as_index=False)["UserTime(s)"].agg(["min", "max", "mean", "std"]))
-
-    # exclude experiments that failed with output limit exception
-    grouped = grouped[
-        (grouped["Binary"] != "patgen") |
-        (grouped["Profile"] == "cshyphen.in") |
-        (grouped["Profile"] == "wortliste.in") |
-        (grouped["Dataset"] != "de_wortliste")
-    ].round({"std": 2})
-
-    grouped.to_csv("time.csv", index=False)
+    process_column(data, "UserTime(s)", "time")
 
 def process_memory(data: pd.DataFrame):
-    grouped = pd.DataFrame(data.groupby(["Binary", "Profile", "Dataset"], as_index=False)["PeakMemory(KB)"].agg(["min", "max", "mean", "std"]))
-    
-    # exclude experiments that failed with output limit exception
-    grouped = grouped[
-        (grouped["Binary"] != "patgen") |
-        (grouped["Profile"] == "cshyphen.in") |
-        (grouped["Profile"] == "wortliste.in") |
-        (grouped["Dataset"] != "de_wortliste")
-    ].round({"std": 2})
-    
-    grouped.to_csv("memory.csv", index=False)
+    process_column(data, "PeakMemory(KB)", "memory")
 
 def check_correctness(data: pd.DataFrame):
     grouped = data.groupby(["Profile", "Dataset"], as_index=False)[["Good", "Bad", "Missed", "Patterns"]].agg(
