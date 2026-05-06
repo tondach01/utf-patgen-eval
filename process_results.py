902bb9f682a2c9a52572e9d133c8e1d149af8e58
Ondřej Metelka <xmetelk1@fi.muni.cz>
2026-05-06 08:55:43 +0200

    fix: sort by file size

--- process_results.py
+++ process_results.py
@@ -47,9 +47,9 @@ def process_column(data: pd.DataFrame,
 def boxplot(data: pd.DataFrame, export_name):
     _, axes = plt.subplots(nrows=1, ncols=2, gridspec_kw={'width_ratios': [1.5, 1]})
 
-    size_order = ["ms_wiktionary", "tr_wiktionary", "uk_wiktionary", "el_wiktionary", "th_orchid", "pt_enwiktionary",
-    "cs_wiktionary", "it_wiktionary", "cs_ujc", "pl_enwiktionary", "ru_wiktionary", "is_hyphis", "nl_wiktionary",
-    "cs_cstenten", "de_wortliste", "de_wiktionary", "es_wiktionary", "cssk_cshyphen"]
+    size_order = ["ms_wiktionary", "tr_wiktionary", "uk_wiktionary", "pt_enwiktionary", "el_wiktionary","cs_wiktionary",
+    "it_wiktionary", "cs_ujc", "pl_enwiktionary", "is_hyphis", "th_orchid", "ru_wiktionary", "nl_wiktionary", "cs_cstenten", 
+    "de_wortliste", "cssk_cshyphen", "es_wiktionary", "de_wiktionary"]
 
     for ax in axes:
         ax.axhline(y=1.0, color='red', linestyle='--', linewidth=0.7, alpha=0.7)
