ac4a6a2ca20440a425c64d413ea90dc317434571
tondach01 <82090595+tondach01@users.noreply.github.com>
2026-05-10 21:05:34 +0200

    refactor: add note about binary paths

--- evaluate.sh
+++ evaluate.sh
@@ -15,7 +15,8 @@ if ! systemctl --user is-system-running &>/dev/null && [ "$(systemctl --user is-
     exit 1
 fi
 
-BINARIES=("/packages/run.64/texlive-2025/bin/patgen" "../utf-patgen/build/utfpatgen")
+# IMPORTANT: Specify the correct paths here!
+BINARIES=("patgen" "../utf-patgen/build/utfpatgen")
 PROFILES_DIR=${PROFILES_DIR:-profiles}
 DATA_DIR=${DATA_DIR:-data}
 OUTPUT_FILE=${1:-evaluation_results.csv}
